b0af644bd253b686eea7c7f71045d5e8bfbcf491
liuyunyun123 <137998494@qq.com>
2018-12-26 17:56:18 +0800

    412
    5231

---
+++ src/main/java/org/lyy/miaosha/SpringbootDemoMiaoshaApplication.java
@@ -0,0 +1,14 @@
+package org.lyy.miaosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoMiaoshaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoMiaoshaApplication.class, args);
+	}
+
+}
+
